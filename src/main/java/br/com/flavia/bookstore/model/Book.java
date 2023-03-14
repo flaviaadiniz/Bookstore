c4dcfac2f67c9132ee39dfb24ae168e03cd1619e
flaviaadiniz <flaviaadiniz@gmail.com>
2023-03-13 22:16:09 -0300

    Created Book entity class and Enum for Book genres

--- src/main/java/br/com/flavia/bookstore/model/Book.java
+++ src/main/java/br/com/flavia/bookstore/model/Book.java
@@ -1,4 +1,27 @@
 package br.com.flavia.bookstore.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="Books")
+@Data
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String year;
+
+    @Column(nullable = false)
+    private Genre genre;
+
 }
---
+++ src/main/java/br/com/flavia/bookstore/model/Genre.java
@@ -0,0 +1,15 @@
+package br.com.flavia.bookstore.model;
+
+public enum Genre {
+
+    BIOGRAPHY,
+    FANTASY,
+    HISTORICAL_FICTION,
+    NOVEL,
+    POETRY,
+    SCIENCE_FICTION,
+    SELF_HELP,
+    SHORT_STORY,
+    THRILLER
+
+}
