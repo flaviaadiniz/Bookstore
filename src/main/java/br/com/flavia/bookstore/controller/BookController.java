4a07b78d26263b643797b9ce9bcf890cdc9bd27a
flaviaadiniz <flaviaadiniz@gmail.com>
2023-03-19 14:15:21 -0300

    Created basic CRUD methods for Book class

--- src/main/java/br/com/flavia/bookstore/controller/BookController.java
+++ src/main/java/br/com/flavia/bookstore/controller/BookController.java
@@ -3,7 +3,10 @@ package br.com.flavia.bookstore.controller;
 import br.com.flavia.bookstore.model.Book;
 import br.com.flavia.bookstore.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -14,8 +17,24 @@ public class BookController {
     private final BookService bookService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Book save(@RequestBody Book book) {
         return bookService.save(book);
+    }
+
+    @GetMapping
+    public Iterable<Book> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> findById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 
 }
--- src/main/java/br/com/flavia/bookstore/service/BookService.java
+++ src/main/java/br/com/flavia/bookstore/service/BookService.java
@@ -5,6 +5,8 @@ import br.com.flavia.bookstore.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -15,4 +17,28 @@ public class BookService {
     public Book save(Book book) {
         return bookRepository.save(book);
     }
+
+    public Iterable<Book> findAll() {
+            return bookRepository.findAll();
+    }
+
+    public Optional<Book> findById(Long id) {
+        if (bookRepository.existsById(id)) {
+            return bookRepository.findById(id);
+        } else {
+
+            System.out.println("ID inexistente!");
+            return Optional.empty();
+        }
+    }
+
+    public void deleteById(Long id) {
+        if (bookRepository.existsById(id)) {
+            bookRepository.deleteById(id);
+        } else {
+            System.out.println("ID inexistente!");
+        }
+    }
+
+
 }
