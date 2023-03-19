720161aa883f725bf886c1cb09b73a28ee3c6e68
flaviaadiniz <flaviaadiniz@gmail.com>
2023-03-19 14:16:21 -0300

    Added Response Status for POST, PUT and DELETE

--- src/main/java/br/com/flavia/bookstore/controller/ClientController.java
+++ src/main/java/br/com/flavia/bookstore/controller/ClientController.java
@@ -3,6 +3,7 @@ package br.com.flavia.bookstore.controller;
 import br.com.flavia.bookstore.model.Client;
 import br.com.flavia.bookstore.service.ClientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -15,6 +16,7 @@ public class ClientController {
     private final ClientService clientService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Client save(@RequestBody Client client) {
         return clientService.save(client);
     }
@@ -51,12 +53,14 @@ public class ClientController {
     }
 
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
     public Client updateClient(@PathVariable Long id, @RequestBody Client client) {
         return clientService.editById(id, client);
     }
 
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable Long id) {
         clientService.deleteById(id);
     }
